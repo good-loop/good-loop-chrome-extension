a38da797fe2ba36fdcdf50f194acd646a557f469
Daniel Winterstein <daniel@good-loop.com>
2020-08-27 17:46:11 +0100

    gl-logo

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,17 +1,19 @@
 
 import $ from 'jquery';
-import _ from 'lodash';
+// import _ from 'lodash';
 
 const LOGTAG = "GL-extension";
 
 console.log(LOGTAG, "Hello :)");
 
-let uOptions = chrome.runtime.getURL('options.html');
-let uLogo = chrome.runtime.getURL('img/logo.64.png');
+// let uOptions = chrome.runtime.getURL('options.html'); doesnt work
+let uLogo = chrome.extension.getURL('/img/logo.64.png'); //chrome.runtime.getURL('img/logo.64.png');
+let uLogo2 = chrome.extension.getURL('img/logo.64.png');
 
-$('body').prepend(
-`<div>
-	<img src='${uLogo}' />FOO<a href='${uOptions}' class='btn btn-secondary'>Go to options</a>
-	Humbug: ${localStorage.getItem("humbug")}
-	<iframe src='https://as.good-loop.com' />
+let h='100px';
+
+$('body').append(
+`<div style='position:fixed; bottom:0; left:0; right:0; height:${h}; background:black;'>
+	<a href='https://my.good-loop.com'><img src='' /></a>
+	<iframe src='https://as.good-loop.com' style='${h}; width:80vw;' />
 </div>`);
