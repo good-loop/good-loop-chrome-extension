2f0b47dec3295c85a162365007668b9134a46ab7
Daniel Winterstein <daniel@good-loop.com>
2020-08-28 17:16:13 +0100

    doc

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # good-loop-chrome-extension
+
 A chrome browser extension for Good-Loop and My-Loop
 
-	// publish here https://chrome.google.com/webstore/devconsole/
+To install from a local git checkout:
+
+1. `npm i`, `npm run compile` (or `watch.sh`)
+2. Put <chrome://extensions/> in your browser
+3. ...Switch on Developer Mode (it's a toggle in the top-right)
+4. ...Click on "Load Unpacked"
+5. ...Pick the `good-loop-chrome-extension/extension` folder to load.
+
+You can test it by opening a new tab.
+
+It should also nobble annoying consent scripts :)
+
+Publish here: https://chrome.google.com/webstore/devconsole/
 
 ## Future Work
-
-
-	// "content_scripts": [		
-	// 	{			
-	// 		"matches": ["<all_urls>"],
-	// 		"exclude_matches": ["*://as.good-loop.com/*","*://my.good-loop.com/*","*://good-loop.com/*"],
-	// 		"js": ["build/js/bundle-debug.js"]
-	// 	}
-	// ],
-
 
 Cross-browser - see: 
 https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions
--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -6,7 +6,7 @@ import {addScript} from './base/utils/miscutils';
 
 const LOGTAG = "GL-extension";
 
-console.log(LOGTAG, "Hello :)");
+console.log(LOGTAG, "Hello :)", window, document);
 
 // let uOptions = chrome.runtime.getURL('options.html'); doesnt work
 let uLogo = chrome.extension.getURL('/img/logo.64.png'); //chrome.runtime.getURL('img/logo.64.png');
@@ -60,10 +60,45 @@ let MYGL = 'https://testmy.good-loop.com';
 // 	<iframe src='${MYGL}/banner.html' style='float:right; height:${h}; width:calc(100vw - 100px);' />
 // </div>`);
 
+function injectScript(func) {
+    let actualCode = '(' + func + ')();'
+	let script = document.createElement('script');
+	script.textContent = actualCode;
+	(document.head||document.documentElement).appendChild(script);
+	script.remove();
+};
+
+injectScript(function() {
+	console.log("HELLO FROM THE PAGE :)", window, document);
+
+	let fn = function (...args) {
+		console.warn("My CMP", args);
+	};
+
+	Object.defineProperty(window, "__tcfapi",
+		{
+			value: fn,
+			writable: false,
+			enumerable: false,
+			configurable: false
+		}
+	);
+
+	Object.defineProperty(window, "__cmp",
+		{
+			value: fn,
+			writable: false,
+			enumerable: false,
+			configurable: false
+		}
+	);
+
+	console.warn("window.__cmp", window.__cmp);
+});
 
 // TODO: add "script.js" to web_accessible_resources in manifest.json
 let uInject = chrome.runtime.getURL('inject.js');
-addScript(uInject);
+// addScript(uInject, {}); gives bundle-debug.js:39036 GET chrome-extension://nnfmfpopejpaniphkkmaeegcpnmmmhlh/inject.js net::ERR_FILE_NOT_FOUND
 
 function storeSet(key,val,callback) {
 	chrome.extension.sendMessage({action:"setstorage"}, function(response){
