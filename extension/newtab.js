cb4d372fea181c07ace0eabf94ad484927f95a4e
dodo721 <dodoworkspr@gmail.com>
2020-11-05 08:48:05 -0800

    Added server switching

--- extension/newtab.js
+++ extension/newtab.js
@@ -4,7 +4,7 @@ let url = kvstore.get("tabUrl");
 // if ( ! url) {
 // 	url = ;
 // 	kvstore.set("tabUrl", url);
-// } 
+// }
 
 console.log("urk",url);
 // console.log("contentscript Chrome storage",chrome.storage);
@@ -16,3 +16,27 @@ console.log("urk",url);
 // console.log("Local storage - set 2");
 
 document.getElementById('tab').setAttribute('src', url);
+
+const reloadTab = () => {
+    let serverType = document.getElementById('serverToggle').value;
+    let urlConfig = {
+        prod:{
+            prefix:"",
+            protocol:"https"
+        },
+        test:{
+            prefix:"test",
+            protocol:"https"
+        },
+        local:{
+            prefix:"local",
+            protocol:"http"
+        }
+    }[serverType];
+
+    url = urlConfig.protocol + "://" + urlConfig.prefix + "my.good-loop.com/newtab.html";
+    console.log("Reloading page on " + serverType);
+    document.getElementById('tab').setAttribute('src', url);
+};
+
+document.getElementById('serverToggle').addEventListener('change', reloadTab);
