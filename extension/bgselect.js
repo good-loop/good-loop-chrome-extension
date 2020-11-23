ff9e3f996b3701d43de0dc8ccf1bb421b7251f89
dodo721 <dodoworkspr@gmail.com>
2020-11-23 08:28:18 -0800

    Image updates

---
+++ extension/bgselect.js
@@ -0,0 +1,5 @@
+var bgImgs = 4;
+var bg = "/img/bg" + (Math.round(Math.random() * bgImgs) + 1) + ".jpg";
+document.getElementById("bg").style.background = "url(" + bg + ")";
+// Was overriden when putting in statically for some reason
+document.getElementById("bg").style.backgroundSize = "cover";
