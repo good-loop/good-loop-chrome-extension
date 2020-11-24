63b3c20e1631c8c39ee46794b078bd3fa412a59f
dodo721 <dodoworkspr@gmail.com>
2020-11-24 03:02:42 -0800

    Updated manifest and images

--- extension/bgselect.js
+++ extension/bgselect.js
@@ -1,4 +1,4 @@
-var bgImgs = 4;
+var bgImgs = 9;
 var bg = "/img/bg" + (Math.round(Math.random() * bgImgs) + 1) + ".jpg";
 document.getElementById("bg").style.background = "url(" + bg + ")";
 // Was overriden when putting in statically for some reason
