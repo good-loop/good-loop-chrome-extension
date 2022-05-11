80cdf35d7a6cf5e4571e6754f8fe55c55c94a3a0
dodo721 <dodoworkspr@gmail.com>
2022-05-11 17:20:11 +0100

    Removed backdrop inserts

--- extension/bgselect.js
+++
@@ -1,5 +0,0 @@
-var bgImgs = 9;
-var bg = "/img/bg" + (Math.round(Math.random() * bgImgs) + 1) + ".jpg";
-document.getElementById("bg").style.background = "url(" + bg + ")";
-// Was overriden when putting in statically for some reason
-document.getElementById("bg").style.backgroundSize = "cover";
