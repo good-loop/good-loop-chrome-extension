8f0813cc8410c79871c91834ba7ddddc2f0b30a7
dodo721 <dodoworkspr@gmail.com>
2022-05-13 15:14:53 +0100

    Exit survey link

--- extension/background.js
+++ extension/background.js
@@ -1,2 +1,2 @@
 
-chrome.runtime.setUninstallURL("https://my.good-loop.com");
+chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSf4vkHY-t4CA56phbYgev0ucwotWVu6PkcjI862fwyr7uNktw/viewform?usp=sf_link");
