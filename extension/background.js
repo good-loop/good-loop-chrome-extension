5fe28324f4c85ab2f886ab025c687f7fb8eb3d9b
Wing Wong <me@nikowong.com>
2022-08-04 12:56:48 +0100

    newtab oninstalled

--- extension/background.js
+++ extension/background.js
@@ -1,2 +1,8 @@
+chrome.runtime.setUninstallURL(
+  'https://docs.google.com/forms/d/e/1FAIpQLSf4vkHY-t4CA56phbYgev0ucwotWVu6PkcjI862fwyr7uNktw/viewform?usp=sf_link'
+);
 
-chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSf4vkHY-t4CA56phbYgev0ucwotWVu6PkcjI862fwyr7uNktw/viewform?usp=sf_link");
+// redirect to newtab when extension is installed
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.create({ url: 'https://my.good-loop.com/newtab.html' + '?utm_source=installation' });
+});
